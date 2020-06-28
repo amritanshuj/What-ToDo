5ceb9997685b19951e0f87546dcf1db495b61845
Amritanshu <amritanshuj.ic.16@nsit.net.in>
2020-06-28 19:27:12 +0530

    Set up the view engine

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const port = 8000;
 // Use Express Router
 app.use('/', require('./routes/index'));
 
+// Set up the viw engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
 app.listen(port, function(err){
     if(err){
         console.log(`Error in running the server: ${port}`);
--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const homeController = require('../controllers/home_controller');
 console.log("Router loaded");
 
 router.get('/', homeController.home);
-
+router.use('/users', require('./users'));
 
 
 module.exports = router;
