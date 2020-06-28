95212c2ba4ce4e4f5c6adb4b9efb2a6b006ed9c6
Amritanshu <amritanshuj.ic.16@nsit.net.in>
2020-06-28 15:11:47 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+
+
+app.listen(port, function(err){
+    if(err){
+        console.log(`Error in running the server: ${port}`);
+    }
+
+    console.log(`Server is runing on port: ${port}`);
+});
